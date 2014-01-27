0f2cd5dcd96988311124a8c21783254ac927d35c
David Buezas <david.buezas@gmail.com>
2014-01-27 20:36:31 +0100

    Moved Plane.coffee (and dependencies) to their own folder in preparation for repository split

--- client/libs/Plane/unmatrix.js
+++ client/libs/Plane/unmatrix.js
@@ -2,21 +2,6 @@
 Copyright (c) 2014 David Buezas. Based on jquery unmatrix plugin (from Stanislav Sopov https://github.com/stassop/unmatrixk) and d3.vector.js 
 (Jason Davies) both of which are based on http://dev.w3.org/csswg/css3-transforms/#matrix-decomposing, 
 which in term is based on Graphics Gems II, edited by Jim Arvo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or 
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT 
-OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 (function(S){
