efbe28740ea8f70ff987864e538b509e4254a632
David Buezas <david.buezas@gmail.com>
2014-01-27 21:41:06 +0100

    cleaned unmatrix.js and used Sylvester Vector library directly

--- client/libs/Plane/unmatrix.js
+++ client/libs/Plane/unmatrix.js
@@ -4,7 +4,7 @@ Copyright (c) 2014 David Buezas. Based on jquery unmatrix plugin (from Stanislav
 which in term is based on Graphics Gems II, edited by Jim Arvo
 */
 
-(function(S){
++function(){
 
     // Returns null if the matrix cannot be decomposed, an object if it can.
     window.unmatrix = function(matrix) {
@@ -46,7 +46,7 @@ which in term is based on Graphics Gems II, edited by Jim Arvo
 
         perspectiveMatrix[3][3] = 1;
 
-        if (determinant(perspectiveMatrix) == 0) {
+        if (Sylvester.$M(perspectiveMatrix).determinant() == 0) {
            return null;
         }
 
@@ -61,9 +61,10 @@ which in term is based on Graphics Gems II, edited by Jim Arvo
             rightHandSide[3] = matrix[3][3];
 
             // Solve the equation by inverting perspectiveMatrix and multiplying rightHandSide by the inverse.
-            inversePerspectiveMatrix = inverse(perspectiveMatrix);
-            transposedInversePerspectiveMatrix = transpose(inversePerspectiveMatrix);
-            perspective = multiplyVectorMatrix(rightHandSide, transposedInversePerspectiveMatrix);
+            inversePerspectiveMatrix = Sylvester.$M(perspectiveMatrix).inverse().elements;
+            transposedInversePerspectiveMatrix = Sylvester.$M(inversePerspectiveMatrix).transpose().elements;
+
+            perspective = Sylvester.$M(transposedInversePerspectiveMatrix).multiply(rightHandSide).elements;
             
             // Clear the perspective partition.
             matrix[0][3] = matrix[1][3] = matrix[2][3] = 0;
@@ -93,35 +94,35 @@ which in term is based on Graphics Gems II, edited by Jim Arvo
         }
 
         // Compute X scale factor and normalize first row.
-        d.scale.x = length(row[0]);
-        row[0] = normalize(row[0]);
+        d.scale.x = Sylvester.$V(row[0]).modulus();
+        row[0] = Sylvester.$V(row[0]).toUnitVector().elements;
 
         // Compute XY shear factor and make 2nd row orthogonal to 1st.
-        skew = dot(row[0], row[1]);
+        skew = Sylvester.$V(row[0]).dot(row[1]);
         row[1] = combine(row[1], row[0], 1.0, -skew);
 
         // Now, compute Y scale and normalize 2nd row.
-        d.scale.y = length(row[1]);
-        row[1] = normalize(row[1]);
+        d.scale.y = Sylvester.$V(row[1]).modulus();
+        row[1] = Sylvester.$V(row[1]).toUnitVector().elements;
         skew /= d.scale.y;
         
         // Compute XZ and YZ shears, orthogonalize 3rd row.
-        d.skew.x = dot(row[0], row[2]);
+        d.skew.x = Sylvester.$V(row[0]).dot(row[2]);
         row[2] = combine(row[2], row[0], 1.0, -d.skew.x);
-        d.skew.y = dot(row[1], row[2]);
+        d.skew.y = Sylvester.$V(row[1]).dot(row[2]);
         row[2] = combine(row[2], row[1], 1.0, -d.skew.y);
 
         // Next, get Z scale and normalize 3rd row.
-        d.scale.z = length(row[2]);
-        row[2] = normalize(row[2]);
+        d.scale.z = Sylvester.$V(row[2]).modulus();
+        row[2] = Sylvester.$V(row[2]).toUnitVector().elements;
         d.skew.x /= d.scale.z;
         d.skew.y /= d.scale.z;
         
         // At this point, the matrix (in rows) is orthonormal. Check for a coordinate system flip. If the 
         // determinant is -1, then negate the matrix and the scaling factors.
-        var pdum3 = cross(row[1], row[2]);
+        var pdum3 = Sylvester.$V(row[1]).cross(row[2]).elements;
         
-        if (dot(row[0], pdum3) < 0) {
+        if (Sylvester.$V(row[0]).dot(pdum3) < 0) {
             for (var i = 0; i < 3; i++) {
                 d.scale.x *= -1;
                 row[i][0] *= -1;
@@ -147,46 +148,6 @@ which in term is based on Graphics Gems II, edited by Jim Arvo
     function deg(rad) {
         return rad * (180 / Math.PI);
     }
-
-    // Returns determinant of matrix. 
-    function determinant(matrix) {
-        return S.Matrix(matrix).determinant();
-    }         
-    
-    // Returns inverse of matrix. 
-    function inverse(matrix) {
-        return S.Matrix(matrix).inverse().elements;
-    }         
-
-    // Returns transpose of matrix. 
-    function transpose(matrix) {
-        return S.Matrix(matrix).transpose().elements;
-    }
-
-    // Multiplies vector by matrix and returns transformed vector.
-    function multiplyVectorMatrix(vector, matrix) {
-        return S.Matrix(matrix).multiply(vector).elements;
-    }
-
-    // Returns length of vector. 
-    function length(vector) {
-        return S.Vector(vector).modulus();
-    }   
-
-    // Normalizes length of vector to 1. 
-    function normalize(vector) {
-        return S.Vector(vector).toUnitVector().elements;
-    }
-
-    // Returns dot product of points. 
-    function dot(vector1, vector2) {
-        return S.Vector(vector1).dot(vector2);
-    }
-
-    // Returns cross product of vectors.
-    function cross(vector1, vector2) {
-        return S.Vector(vector1).cross(vector2).elements;
-    }
     
     function combine(a, b, ascl, bscl) {
         var result = new Array();
@@ -199,5 +160,4 @@ which in term is based on Graphics Gems II, edited by Jim Arvo
         return result;
     }
 
-
-})(Sylvester)
+}()
